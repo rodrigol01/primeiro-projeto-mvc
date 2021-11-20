ed0665927fe4dcfa92a896e0d2b083128d18a15a
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2021-11-20 15:35:47 -0300

    incluindo render dinamico com nvelocity

--- MVC_GerenciadorDeConteudo/Controllers/PaginasController.cs
+++ MVC_GerenciadorDeConteudo/Controllers/PaginasController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Web.Mvc;
 using Business;
+using NVelocity;
+using NVelocity.App;
 
 namespace MVC_GerenciadorDeConteudo.Controllers
 {
@@ -42,6 +46,33 @@ namespace MVC_GerenciadorDeConteudo.Controllers
         {
             var pagina = Pagina.BuscaPorId(id);
             ViewBag.Pagina = pagina;
+            return View();  
+        }
+        
+        public ActionResult PreviewDinamico(int id)
+        {
+            var pagina = Pagina.BuscaPorId(id);
+            Velocity.Init();
+
+            var modelo = new
+            {
+                Header = "lista de dados dinamicos",
+                Itens = new[]
+                {
+                    new {ID = 1, Nome = "Texto 001", Negrito = false},
+                    new {ID = 2, Nome = "Texto 002", Negrito = true},
+                    new {ID = 3, Nome = "Texto 003", Negrito = false},
+                }
+            };
+
+            var velocityContext = new VelocityContext();
+            velocityContext.Put("model", modelo);
+
+            var html = new StringBuilder();
+            bool result = Velocity.Evaluate(velocityContext, new StringWriter(html), "NomeParaCapturarLogError"
+                , new StringReader(pagina.Conteudo));
+
+            ViewBag.Html = html.ToString();
             return View();
         }
         
